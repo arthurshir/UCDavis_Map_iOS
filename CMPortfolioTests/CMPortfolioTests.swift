70339496c64d5e1af781efd7afb2f53532b1a6df
fantaandcrackers <arthur.shir@gmail.com>
2015-09-25 03:02:00 -0700

    Initial Commit

---
+++ CMPortfolioTests/CMPortfolioTests.swift
@@ -0,0 +1,36 @@
+//
+//  CMPortfolioTests.swift
+//  CMPortfolioTests
+//
+//  Created by Arthur Shir on 9/25/15.
+//  Copyright (c) 2015 Aashir. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CMPortfolioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
